5f3db5cb9ddce6fda274957d5be948f70106a409
yyaayyaatt <yyaayyaatt.kawe@gmail.com>
2025-05-29 01:51:36 +0700

    Add List Barang Retail Activity

--- app/src/main/java/com/yyaayyaatt/merchantstore/ListRetailsActivity.java
+++ app/src/main/java/com/yyaayyaatt/merchantstore/ListRetailsActivity.java
@@ -1,14 +1,91 @@
 package com.yyaayyaatt.merchantstore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.yyaayyaatt.merchantstore.adapter.ProdukRecylerAdapter;
+import com.yyaayyaatt.merchantstore.adapter.ProdukRetailRecylerAdapter;
+import com.yyaayyaatt.merchantstore.model.Kategori;
+import com.yyaayyaatt.merchantstore.model.Produk;
+import com.yyaayyaatt.merchantstore.model.ResponseProduk;
+import com.yyaayyaatt.merchantstore.service.BaseApiService;
+import com.yyaayyaatt.merchantstore.service.SharedPrefManager;
+import com.yyaayyaatt.merchantstore.service.UtilsApi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ListRetailsActivity extends AppCompatActivity {
 
+    BaseApiService mApiService;
+    SharedPrefManager sharedPrefManager;
+    ProgressDialog progressDialog;
+    Context mContext;
+    List<Produk> produks = new ArrayList<>();
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView mRecycler;
+
+    private RecyclerView.LayoutManager mLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_retails);
+        mRecycler = findViewById(R.id.rv_retail);
+        mContext = ListRetailsActivity.this;
+        progressDialog = new ProgressDialog(mContext);
+        mApiService = UtilsApi.getAPIService();
+        sharedPrefManager = new SharedPrefManager(mContext);
+
+        mRecycler.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(mContext);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            mRecycler.setLayoutManager(new GridLayoutManager(mContext, 1, RecyclerView.VERTICAL, false));
+        } else {
+            mRecycler.setLayoutManager(new GridLayoutManager(mContext, 2, RecyclerView.VERTICAL, false));
+        }
+        progressDialog.setMessage("Tunggu...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        getProduks("", "Retail");
+
+    }
+    private void getProduks(String key, String kat) {
+        produks.clear();
+        Call<ResponseProduk> getdata = mApiService.getProduks(key, kat);
+        getdata.enqueue(new Callback<ResponseProduk>() {
+            @Override
+            public void onResponse(Call<ResponseProduk> call, Response<ResponseProduk> response) {
+                if (response.isSuccessful()) {
+                    progressDialog.dismiss();
+                    if (response.body().getmKode().equals("1")) {
+                        produks = response.body().getResult();
+                        if (!produks.isEmpty()) {
+                            mAdapter = new ProdukRetailRecylerAdapter(mContext, produks);
+                            mRecycler.setAdapter(mAdapter);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseProduk> call, Throwable t) {
+                Log.e("debug", "onFailure: ERROR > " + t.toString());
+                progressDialog.dismiss();
+            }
+        });
     }
 }
